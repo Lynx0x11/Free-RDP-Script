5218b6b5704e7df7484dd474ae61ce2c0ab1a91a
GOBINATH <84310276+d4rkd3v1l1702@users.noreply.github.com>
2024-03-24 12:10:18 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,29 +1,4 @@
-# Disable firewall profiles
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-
-# Define URLs for Chrome Remote Desktop and other installers
-$ChromeRemoteDesktopURL = 'https://github.com/d4rkd3v1l1702/Free-RDP-Script/raw/main/chromeremotedesktophost.msi'
-$ChromeInstallerURL = 'https://dl.google.com/chrome/install/latest/chrome_installer.exe'
-$WingetInstallerURL = 'https://aka.ms/getwinget'
-
-# Download and install Chrome Remote Desktop
-$ChromeRemoteDesktopInstaller = $env:TEMP + '\chromeremotedesktophost.msi'
-Invoke-WebRequest -Uri $ChromeRemoteDesktopURL -OutFile $ChromeRemoteDesktopInstaller
-Start-Process $ChromeRemoteDesktopInstaller -Wait
-Remove-Item $ChromeRemoteDesktopInstaller
-
-# Download and install Google Chrome
-$ChromeInstaller = $env:TEMP + '\chrome_installer.exe'
-Invoke-WebRequest -Uri $ChromeInstallerURL -OutFile $ChromeInstaller
-Start-Process -FilePath $ChromeInstaller -Args '/install' -Verb RunAs -Wait
-Remove-Item $ChromeInstaller
-
-# Download and install Winget
-$WingetInstaller = $env:TEMP + '\winget.msixbundle'
-Invoke-WebRequest -Uri $WingetInstallerURL -OutFile $WingetInstaller
-Add-AppxPackage -Path $WingetInstaller
-Start-Sleep -Seconds 10  # Wait for Winget installation to finish (optional delay)
-Remove-Item $WingetInstaller
-
-# Install NirCmd using Winget
-Start-Process -FilePath "winget" -ArgumentList 'install', '-e', '--id', 'NirSoft.NirCmd' -Wait
+& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://github.com/d4rkd3v1l1702/Free-RDP-Script/blob/main/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\winget.msixbundle'; Invoke-WebRequest 'https://aka.ms/getwinget' -OutFile $P; Add-AppxPackage -Path $P; Start-Process winget install -e --id NirSoft.NirCmd}
