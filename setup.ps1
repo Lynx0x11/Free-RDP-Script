b88068fb390b19c6939552d87d809a6e5c6a8e0a
GOBINATH <84310276+d4rkd3v1l1702@users.noreply.github.com>
2024-03-24 11:02:39 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,20 @@
+# Disable firewall profiles
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\winget.msixbundle'; Invoke-WebRequest 'https://aka.ms/getwinget' -OutFile $P; Add-AppxPackage -Path $P; Start-Process winget install -e --id NirSoft.NirCmd}
+
+# Download and install Chrome Remote Desktop
+$ChromeRemoteDesktopInstaller = $env:TEMP + '\chromeremotedesktophost.msi'
+Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $ChromeRemoteDesktopInstaller
+Start-Process $ChromeRemoteDesktopInstaller -Wait
+Remove-Item $ChromeRemoteDesktopInstaller
+
+# Download and install Google Chrome
+$ChromeInstaller = $env:TEMP + '\chrome_installer.exe'
+Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $ChromeInstaller
+Start-Process -FilePath $ChromeInstaller -Args '/install' -Verb RunAs -Wait
+Remove-Item $ChromeInstaller
+
+# Download and install Winget
+$WingetInstaller = $env:TEMP + '\winget.msixbundle'
+Invoke-WebRequest 'https://aka.ms/getwinget' -OutFile $WingetInstaller
+Start-Process -FilePath "winget.exe" -ArgumentList 'install', '-e', '--id', 'NirSoft.NirCmd' -Wait
+Remove-Item $WingetInstaller
