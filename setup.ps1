fd034bfa66c197756418caf0e159bbd1c3282b7d
GOBINATH <84310276+d4rkd3v1l1702@users.noreply.github.com>
2024-03-24 11:45:10 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = "$env:TEMP\chromeremotedesktophost.msi"
-        Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/current/chromeremotedesktophost.msi' -OutFile $P
+        Invoke-WebRequest 'https://github.com/d4rkd3v1l1702/Free-RDP-Script/blob/main/chromeremotedesktophost.msi' -OutFile $P
         Start-Process $P -Wait
         Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
